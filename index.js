5aa8c851da83272eb48f159cd5ae9495338484ef
huise0ng <shinhuiseong07@gmail.com>
2023-11-29 15:18:36 +0900

    ADD :: 웹이랑 연결

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 const mysql = require('mysql');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// 시리얼 포트 설정
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 const portSettings = {
-    path: 'COM10',      // 시리얼 포트 경로
-    baudRate: 115200    // 보드 레이트 (통신 속도)
+    path: 'COM10',      
+    baudRate: 115200    
 };
 const port = new SerialPort(portSettings);
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-
 const db = mysql.createConnection({
-    host: "localhost",       
-    user: "root",            
-    password: "070927",      
-    database: "shs"         
+    host: "localhost",
+    user: "root",
+    password: "070927",
+    database: "shs"
 });
-
 
 db.connect(err => {
     if (err) {
@@ -27,12 +34,10 @@ db.connect(err => {
     console.log('Connected to database.');
 });
 
-
 parser.on('data', (data) => {
     console.log(`Received data: ${data}`);
     let cardData = data.trim();
 
-    
     db.query('SELECT * FROM pn532 WHERE uid = ?', [cardData], (err, results) => {
         if (err) {
             console.error(err.message);
@@ -40,14 +45,17 @@ parser.on('data', (data) => {
         }
         if (results.length > 0) {
             console.log('Card matched: ', results[0]);
-            console.log('신희성');
+            io.emit('cardMatch', '신희성'); 
         } else {
             console.log('Card not found');
         }
     });
 });
 
-
 port.on('error', function(err) {
     console.log('Error: ', err.message);
+});
+
+server.listen(3000, () => {
+    console.log('localhost:3000');
 });
