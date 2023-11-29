b0e53fcf4d364a9ffe6ae969bf4721f3b5e5c7db
huise0ng <shinhuiseong07@gmail.com>
2023-11-29 10:20:29 +0900

    DOCS :: 주석 이제 다 필요없닷

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const portSettings = {
 const port = new SerialPort(portSettings);
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-// MySQL 데이터베이스 연결 정보 설정
+
 const db = mysql.createConnection({
-    host: "localhost",       // 데이터베이스 호스트 주소
-    user: "root",            // 데이터베이스 사용자 이름
-    password: "070927",      // 데이터베이스 비밀번호
-    database: "shs"          // 사용할 데이터베이스 이름
+    host: "localhost",       
+    user: "root",            
+    password: "070927",      
+    database: "shs"         
 });
 
-// 데이터베이스 연결
+
 db.connect(err => {
     if (err) {
         console.error('Error connecting to the database: ' + err.stack);
@@ -27,12 +27,12 @@ db.connect(err => {
     console.log('Connected to database.');
 });
 
-// 시리얼 포트로부터 데이터 읽기
+
 parser.on('data', (data) => {
     console.log(`Received data: ${data}`);
     let cardData = data.trim();
 
-    // 데이터베이스에서 카드 정보 조회
+    
     db.query('SELECT * FROM pn532 WHERE uid = ?', [cardData], (err, results) => {
         if (err) {
             console.error(err.message);
@@ -40,14 +40,14 @@ parser.on('data', (data) => {
         }
         if (results.length > 0) {
             console.log('Card matched: ', results[0]);
-            console.log('신희성'); // 일치하는 경우 1 출력
+            console.log('신희성');
         } else {
             console.log('Card not found');
         }
     });h
 });
 
-// 포트 열기 에러 처리
+
 port.on('error', function(err) {
     console.log('Error: ', err.message);
 });
